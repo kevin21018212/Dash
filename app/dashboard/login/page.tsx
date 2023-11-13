84ad90fb169d81289e9f185c18f690ed3a47e7e9
kevin21018212 <trashlinkmain@gmail.com>
2023-11-12 20:50:08 -0600

    signin update

--- app/dashboard/login/page.tsx
+++ app/dashboard/login/page.tsx
@@ -1,21 +1,23 @@
-"use client";
 import React from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
+import styles from "./SigninButton.module.css";
+
 const SigninButton = () => {
   const { data: session } = useSession();
 
   if (session && session.user) {
     return (
-      <div className="flex gap-4 ml-auto">
-        <p className="text-sky-600">{session.user.name}</p>
-        <button onClick={() => signOut()} className="text-red-600">
+      <div className={styles.flexContainer}>
+        <p className={styles.userName}>{session.user.name}</p>
+        <button onClick={() => signOut()} className={styles.signOutButton}>
           Sign Out
         </button>
       </div>
     );
   }
+
   return (
-    <button onClick={() => signIn()} className="text-green-600 ml-auto">
+    <button onClick={() => signIn()} className={styles.signInButton}>
       Sign In
     </button>
   );
