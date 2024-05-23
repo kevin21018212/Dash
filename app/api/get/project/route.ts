e59d32223811ec398f730c844d3a71426ff78c32
A5NHUN2_deere <trashlinkmain@gmail.com>
2024-05-23 11:12:49 -0500

    fixed ingestion

--- app/api/get/project/route.ts
+++ app/api/get/project/route.ts
@@ -1,15 +1,15 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
-import prisma from "@/prisma/prisma";
+import {NextRequest, NextResponse} from 'next/server';
+import {getServerSession} from 'next-auth';
+import prisma from '@/prisma/prisma';
 
-import { findUserByGoogleId } from "@/app/utils/userHelper";
-import { authOptions } from "@/app/utils/authOptions";
+import {findUserByGoogleId} from '@/app/utils/userHelper';
+import {authOptions} from '@/app/utils/authOptions';
 
 export async function GET(req: NextRequest) {
   const session = await getServerSession();
 
   if (!session) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    return NextResponse.json({error: 'Unauthorized'}, {status: 401});
   }
 
   const google_id = session.user?.email as string;
@@ -18,7 +18,7 @@ export async function GET(req: NextRequest) {
     const user = await findUserByGoogleId(google_id);
 
     if (!user) {
-      return NextResponse.json({ error: "User not found" }, { status: 404 });
+      return NextResponse.json({error: 'User not found'}, {status: 404});
     }
 
     const projects = await prisma.project.findMany({
@@ -30,11 +30,8 @@ export async function GET(req: NextRequest) {
       },
     });
 
-    return NextResponse.json({ projects }, { status: 200 });
+    return NextResponse.json({projects}, {status: 200});
   } catch (error) {
-    return NextResponse.json(
-      { error: "Something went wrong" },
-      { status: 500 }
-    );
+    return NextResponse.json({error: 'Something went wrong'}, {status: 500});
   }
 }
--- app/utils/userHelper.ts
+++ app/utils/userHelper.ts
@@ -1,4 +1,5 @@
-import prisma from "@/prisma/prisma";
+import prisma from '@/prisma/prisma';
+import {getServerSession} from 'next-auth';
 
 /**
  * Finds a user by their Google ID (email).
@@ -7,12 +8,22 @@ import prisma from "@/prisma/prisma";
  */
 export async function findUserByGoogleId(google_id: string) {
   try {
-    const user = await prisma.user.findUnique({
-      where: { google_id },
+    let user = await prisma.user.findUnique({
+      where: {google_id},
     });
+    if (!user && google_id) {
+      const session = await getServerSession();
+      user = await prisma.user.create({
+        data: {
+          google_id: session?.user?.email as string,
+          username: session?.user?.name as string,
+        },
+      });
+    }
+
     return user;
   } catch (error) {
-    console.error("Error finding user:", error);
-    throw new Error("Error finding user");
+    console.error('Error finding user:', error);
+    throw new Error('Error finding user');
   }
 }
