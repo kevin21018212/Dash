5ab8db9254975624332352cd418554cb49628994
kevin21018212 <trashlinkmain@gmail.com>
2024-06-18 19:05:13 -0500

    shchema changes

--- app/api/project/route.ts
+++ app/api/project/route.ts
@@ -1,108 +1,130 @@
-import {NextRequest, NextResponse} from 'next/server';
-import {getServerSession} from 'next-auth';
-import prisma from '@/prisma/prisma';
-import {findUserByGoogleId} from '@/app/utils/userHelper';
-import {User} from '@prisma/client';
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import prisma from "@/prisma/prisma";
+import { findUserByGoogleId } from "@/app/utils/userHelper";
+import { User } from "@prisma/client";
 
 async function getSessionAndUser(req: NextRequest) {
   const session = await getServerSession();
   if (!session) {
-    return {error: 'Unauthorized', status: 401, user: null};
+    return { error: "Unauthorized", status: 401, user: null };
   }
 
   const google_id = session.user?.email as string;
   const user = (await findUserByGoogleId(google_id)) as User;
 
   if (!user) {
-    return {error: 'User not found', status: 404, user: null};
+    return { error: "User not found", status: 404, user: null };
   }
 
-  return {error: null, status: 200, user};
+  return { error: null, status: 200, user };
 }
 
 export async function POST(req: NextRequest) {
-  const {error, status, user} = await getSessionAndUser(req);
+  const { error, status, user } = await getSessionAndUser(req);
   if (error) {
-    return NextResponse.json({error}, {status});
+    return NextResponse.json({ error }, { status });
   }
 
   const body = await req.json();
-  const {title, link, description, image_url} = body;
+  const { title, link, description, image_url } = body;
 
   if (!description) {
-    return NextResponse.json({error: 'Description is required'}, {status: 400});
+    return NextResponse.json(
+      { error: "Description is required" },
+      { status: 400 }
+    );
   }
 
   if (user?.user_id) {
     try {
       const newProject = await prisma.project.create({
-        data: {title, link, description, image_url, user_id: user.user_id},
+        data: { title, link, description, image_url, user_id: user.user_id },
       });
 
-      return NextResponse.json({message: 'Project created', project: newProject}, {status: 201});
+      return NextResponse.json(
+        { message: "Project created", project: newProject },
+        { status: 201 }
+      );
     } catch (error) {
-      return NextResponse.json({error: 'Something went wrong'}, {status: 500});
+      return NextResponse.json(
+        { error: "Something went wrong" },
+        { status: 500 }
+      );
     }
   }
 }
 
 export async function PUT(req: NextRequest) {
-  const {error, status, user} = await getSessionAndUser(req);
+  const { error, status, user } = await getSessionAndUser(req);
   if (error) {
-    return NextResponse.json({error}, {status});
+    return NextResponse.json({ error }, { status });
   }
 
-  const {searchParams} = new URL(req.url);
-  const projectId = searchParams.get('projectId') as string;
-  const {title, link, description, image_url} = await req.json();
+  const { searchParams } = new URL(req.url);
+  const projectId = searchParams.get("projectId") as string;
+  const { title, link, description, image_url } = await req.json();
 
   try {
     const updatedProject = await prisma.project.update({
-      where: {project_id: parseInt(projectId)},
-      data: {title, link, description, image_url},
+      where: { project_id: parseInt(projectId) },
+      data: { title, link, description, image_url },
     });
 
-    return NextResponse.json(updatedProject, {status: 200});
+    return NextResponse.json(updatedProject, { status: 200 });
   } catch (error) {
-    return NextResponse.json({error: 'Something went wrong'}, {status: 500});
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
   }
 }
 
 export async function DELETE(req: NextRequest) {
-  const {error, status, user} = await getSessionAndUser(req);
+  const { error, status, user } = await getSessionAndUser(req);
   if (error) {
-    return NextResponse.json({error}, {status});
+    return NextResponse.json({ error }, { status });
   }
 
-  const {searchParams} = new URL(req.url);
-  const projectId = searchParams.get('projectId') as string;
+  const { searchParams } = new URL(req.url);
+  const projectId = searchParams.get("projectId") as string;
 
   try {
     await prisma.project.delete({
-      where: {project_id: parseInt(projectId)},
+      where: { project_id: parseInt(projectId) },
     });
 
-    return NextResponse.json({message: 'Project deleted successfully'}, {status: 200});
+    return NextResponse.json(
+      { message: "Project deleted successfully" },
+      { status: 200 }
+    );
   } catch (error) {
-    return NextResponse.json({error: 'Something went wrong'}, {status: 500});
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
   }
 }
 
 export async function GET(req: NextRequest) {
-  const {error, status, user} = await getSessionAndUser(req);
+  const { error, status, user } = await getSessionAndUser(req);
   if (error) {
-    return NextResponse.json({error}, {status});
+    return NextResponse.json({ error }, { status });
   }
+
   if (user?.user_id) {
     try {
       const projects = await prisma.project.findMany({
-        where: {user_id: user.user_id},
-        include: {features: true},
+        where: { user_id: user.user_id },
+        include: { features: true },
       });
 
-      return NextResponse.json({projects}, {status: 200});
+      return NextResponse.json({ projects }, { status: 200 });
     } catch (error) {
-      return NextResponse.json({error: 'Something went wrong'}, {status: 500});
+      return NextResponse.json(
+        { error: "Something went wrong" },
+        { status: 500 }
+      );
     }
   }
 }
--- components/global/form/create.tsx
+++ components/global/form/create.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 import { useState } from "react";
 import { TaskSize, TaskType } from "@/app/utils/enums";
@@ -6,28 +5,12 @@ import form from "./form.module.scss";
 import common from "@/app/common.module.scss";
 import FormField from "./formField";
 import { motion } from "framer-motion";
-import { useContentHandlers } from "@/app/utils/contentHandlers";
 import { dbProject, dbFeature, dbTask } from "@/app/types";
 
 const CreateComponent = ({ type, parentId, onCancel }) => {
-  const { createProject, createFeature, createTask } = useContentHandlers();
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [imageUrl, setImageUrl] = useState("");
-=======
-'use client';
-import {useState} from 'react';
-import {TaskSize, TaskType} from '@/app/utils/enums';
-import form from './form.module.scss';
-import common from '@/app/common.module.scss';
-import FormField from './formField';
-import {motion} from 'framer-motion';
-
-const CreateComponent = ({type, parentId, onCancel}) => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [imageUrl, setImageUrl] = useState('');
->>>>>>> dea2afbfc1d7747d412eb2587970b9169c009282
   const [taskType, setTaskType] = useState(TaskType.UIDesign);
   const [taskSize, setTaskSize] = useState(TaskSize.Hard);
 
@@ -37,95 +20,77 @@ const CreateComponent = ({type, parentId, onCancel}) => {
     let data: Partial<dbProject & dbFeature & dbTask> = {
       title,
       description,
-<<<<<<< HEAD
-    };
-
-    if (type === "project") {
-      data = {
-        ...data,
-        image_url: imageUrl,
-        link: "",
-      };
-      createProject(data as dbProject, onCancel);
-    } else if (type === "feature") {
-      data = {
-        ...data,
-        project_id: parentId,
-      };
-      createFeature(data as dbFeature, parentId, onCancel);
-    } else if (type === "task") {
-      data = {
-        ...data,
-        image_url: imageUrl,
-        type: taskType,
-        size: taskSize,
-        feature_id: parentId,
-      };
-      createTask(data as dbTask, parentId, onCancel);
-=======
-      ...(type === 'task' && {image_url: imageUrl}),
-      [`${type === 'task' ? 'feature_id' : 'project_id'}`]: parentId,
-      ...(type === 'task' && {type: taskType, size: taskSize}),
+      ...(type === "task" && { image_url: imageUrl }),
+      [`${type === "task" ? "feature_id" : "project_id"}`]: parentId,
+      ...(type === "task" && { type: taskType, size: taskSize }),
     };
 
     const response = await fetch(`/api/${type}`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
 
     if (response.ok) {
-      setTitle('');
-      setDescription('');
-      setImageUrl('');
-      if (type === 'task') {
+      setTitle("");
+      setDescription("");
+      setImageUrl("");
+      if (type === "task") {
         setTaskType(TaskType.UIDesign);
         setTaskSize(TaskSize.Easy);
       }
     } else {
       console.error(`Failed to create ${type}`);
->>>>>>> dea2afbfc1d7747d412eb2587970b9169c009282
     }
   };
 
   return (
     <div>
-      <h3 className={form.title}>Create {type.charAt(0).toUpperCase() + type.slice(1)}</h3>
+      <h3 className={form.title}>
+        Create {type.charAt(0).toUpperCase() + type.slice(1)}
+      </h3>
       <form onSubmit={handleSubmit}>
-        <FormField label='Title' type='text' value={title} onChange={(e) => setTitle(e.target.value)} required options={undefined} />
         <FormField
-          label='Description'
-          type='textarea'
+          label="Title"
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          options={undefined}
+        />
+        <FormField
+          label="Description"
+          type="textarea"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
           required
           options={undefined}
         />
-        {type === 'project' && (
+        {type === "project" && (
           <FormField
-            label='Image URL'
-            type='url'
+            label="Image URL"
+            type="url"
             value={imageUrl}
             onChange={(e) => setImageUrl(e.target.value)}
             options={undefined}
             required={undefined}
           />
         )}
-        {type === 'task' && (
+        {type === "task" && (
           <>
             <FormField
-              label='Task Type'
-              type='select'
+              label="Task Type"
+              type="select"
               value={taskType}
               onChange={(e) => setTaskType(e.target.value)}
               options={Object.values(TaskType)}
               required
             />
             <FormField
-              label='Task Size'
-              type='select'
+              label="Task Size"
+              type="select"
               value={taskSize}
               onChange={(e) => setTaskSize(e.target.value)}
               options={Object.values(TaskSize)}
@@ -134,11 +99,20 @@ const CreateComponent = ({type, parentId, onCancel}) => {
           </>
         )}
         <div className={form.buttonGroup}>
-          <motion.button type='submit' whileHover={{scale: 1.05}} className={common.saveButton}>
+          <motion.button
+            type="submit"
+            whileHover={{ scale: 1.05 }}
+            className={common.saveButton}
+          >
             Create {type.charAt(0).toUpperCase() + type.slice(1)}
           </motion.button>
           {onCancel != null ? (
-            <motion.button type='button' whileHover={{scale: 1.05}} className={common.deleteButton} onClick={onCancel}>
+            <motion.button
+              type="button"
+              whileHover={{ scale: 1.05 }}
+              className={common.deleteButton}
+              onClick={onCancel}
+            >
               Cancel
             </motion.button>
           ) : (
