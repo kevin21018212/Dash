8fbe9b2a48392601701c4ad41799dddd65b2cf32
kevin21018212 <trashlinkmain@gmail.com>
2024-07-14 17:32:46 -0500

    broke up featureContnet editing

---
+++ components/feature/editFeature.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { Feature } from "@/app/types";
+import { EditableField } from "../global/form/edit";
+import styles from "./featureContent.module.scss";
+import { useContentHandlers } from "@/app/utils/contentHandlers";
+
+interface EditFeatureProps {
+  feature: Feature;
+  setEditing: (editing: boolean) => void;
+}
+
+const EditFeature: React.FC<EditFeatureProps> = ({ feature, setEditing }) => {
+  const { handleFieldChange, saveFeature, deleteFeature } =
+    useContentHandlers();
+  const [editedFeature, setEditedFeature] = useState<Feature>(feature);
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.featureInfo}>
+        <EditableField
+          value={editedFeature.title}
+          onSave={(value) =>
+            handleFieldChange("title", value, editedFeature, setEditedFeature)
+          }
+        />
+        <EditableField
+          value={editedFeature.description}
+          onSave={(value) =>
+            handleFieldChange(
+              "description",
+              value,
+              editedFeature,
+              setEditedFeature
+            )
+          }
+          type="textArea"
+        />
+      </div>
+      <div className={styles.actionButtons}>
+        <button
+          onClick={() => {
+            saveFeature(feature, editedFeature, setEditing);
+            setEditing(false); // Assuming you want to close the edit mode on save
+          }}
+          className={styles.saveButton}
+        >
+          Save
+        </button>
+        <button
+          onClick={() => {
+            deleteFeature(feature);
+            setEditing(false); // Assuming you want to close the edit mode on delete
+          }}
+          className={styles.deleteButton}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default EditFeature;
--- components/feature/featureContent.tsx
+++ components/feature/featureContent.tsx
@@ -1,143 +1,33 @@
-import React, { useState, useEffect, useRef } from "react";
-import { useContentHandlers } from "@/app/utils/contentHandlers";
+import React, { useState, useRef } from "react";
 import styles from "./featureContent.module.scss";
-
-import { EditableField } from "../global/form/edit";
-import TaskContent from "../task/taskContent";
-import { FiEdit } from "react-icons/fi";
-import { CreateTask } from "../global/form/create";
-import { motion } from "framer-motion";
-import Modal from "../global/function/modal";
 import { Feature } from "@/app/types";
+import { FiEdit } from "react-icons/fi";
+import EditFeature from "./editFeature";
 
 interface FeatureContentProps {
   feature: Feature;
 }
 
 const FeatureContent: React.FC<FeatureContentProps> = ({ feature }) => {
-  const { handleFieldChange, saveFeature, deleteFeature } =
-    useContentHandlers();
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
-  const [editedFeature, setEditedFeature] = useState<Feature>(feature);
-  const [showCreateTask, setShowCreateTask] = useState(false);
-  const featureRef = useRef<HTMLInputElement>(null);
+  const featureRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        featureRef.current &&
-        !featureRef.current.contains(event.target as Node)
-      ) {
-        setIsEditing(false);
-      }
-    };
-
-    if (isEditing) {
-      document.addEventListener("mousedown", handleClickOutside);
-    } else {
-      document.removeEventListener("mousedown", handleClickOutside);
-    }
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [isEditing]);
+  if (isEditing) {
+    return <EditFeature feature={feature} setEditing={setIsEditing} />;
+  }
 
   return (
-    <>
-      {isEditing ? (
-        <div ref={featureRef} className={styles.card}>
-          <div className={styles.featureInfo}>
-            <div className={styles.titleContainer}>
-              <EditableField
-                value={editedFeature.title}
-                onSave={(value) =>
-                  handleFieldChange(
-                    "title",
-                    value,
-                    editedFeature,
-                    setEditedFeature
-                  )
-                }
-              />
-            </div>
-            <div className={styles.descriptionContainer}>
-              <EditableField
-                value={editedFeature.description}
-                onSave={(value) =>
-                  handleFieldChange(
-                    "description",
-                    value,
-                    editedFeature,
-                    setEditedFeature
-                  )
-                }
-                type="textArea"
-              />
-            </div>
-          </div>
-          <div className={styles.actionButtons}>
-            <button
-              onClick={() => saveFeature(feature, editedFeature, setIsEditing)}
-              className={styles.saveButton}
-            >
-              Save
-            </button>
-            <button
-              onClick={() => deleteFeature(feature)}
-              className={styles.deleteButton}
-            >
-              Delete
-            </button>
-          </div>
+    <div ref={featureRef} className={styles.card}>
+      <div className={styles.featureInfo}>
+        <div className={styles.titleContainer}>
+          <h3 className={styles.title}>{feature.title}</h3>
         </div>
-      ) : (
-        <div ref={featureRef} className={styles.card}>
-          <div className={styles.featureInfo}>
-            <div className={styles.titleContainer}>
-              <h3 className={styles.title}>{feature.title}</h3>
-            </div>
-            <div className={styles.descriptionContainer}>
-              <p className={styles.description}>{feature.description}</p>
-            </div>
-          </div>
-          <motion.div
-            whileHover={{ scale: 1.05 }}
-            className={styles.clickableArea}
-            onClick={() => setIsModalOpen(true)}
-          >
-            <p>Click here to view tasks</p>
-          </motion.div>
-          <FiEdit
-            className={styles.editIcon}
-            onClick={() => setIsEditing(true)}
-          />
-
-          {isModalOpen && (
-            <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-              <div className={styles.taskGrid}>
-                <motion.div
-                  whileHover={{ scale: 1.1 }}
-                  className={styles.createTaskCard}
-                  onClick={() => setShowCreateTask(true)}
-                >
-                  + Create Task
-                </motion.div>
-                {feature.tasks.map((task, index) => (
-                  <TaskContent task={task} key={index} />
-                ))}
-              </div>
-              {showCreateTask && (
-                <CreateTask
-                  parentId={feature.feature_id}
-                  onCancel={() => setIsModalOpen(false)}
-                />
-              )}
-            </Modal>
-          )}
+        <div className={styles.descriptionContainer}>
+          <p className={styles.description}>{feature.description}</p>
         </div>
-      )}
-    </>
+      </div>
+      <FiEdit className={styles.editIcon} onClick={() => setIsEditing(true)} />
+    </div>
   );
 };
 
